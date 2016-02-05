02e8d31e842ef17e4c80c9d1e117a34f3b9b9b51
AliumAstrum <design@spartak.ru>
2016-02-05 13:17:08 +0300

    Изображения услуг

--- pages/service.php
+++ pages/service.php
@@ -9,19 +9,58 @@
 	<center>
 		<div id="services_wrapper">
 			<ul id="services">
-				<li>Асфальтирование дорог и территории</li>
-				<li>Асфальтирование малых площадей</li>
-				<li>Ремонт дачных, коттеджных дорог</li>
-				<li>Ямочный ремонт дорог</li>
-				<li>Стоянка, парковка под ключ</li>
-				<li>Строительство дорог</li>
-				<li>Благоустройство территории</li>
-				<li>Укладка асфальтовой крошки</li>
-				<li>Продажа асфальтовой крошки</li>
-				<li>Технология асфальтирования</li>
-				<li>Технология строительство дорог</li>
-				<li>Технология укладки асфальтовой крошки</li>
-				<li>Технология ремонта дорог</li>
+				<li>
+					<div class="image" style="background-image: url('images/menu_item_01.jpg');"></div>
+					<div class="text">Асфальтирование дорог и территории</div>
+				</li>
+				<li>
+					<div class="image" style="background-image: url('images/menu_item_02.jpg');"></div>
+					<div class="text">Асфальтирование малых площадей</div>
+				</li>
+				<li>
+					<div class="image" style="background-image: url('images/menu_item_03.jpg');"></div>
+					<div class="text">Ремонт дачных, коттеджных дорог</div>
+				</li>
+				<li>
+					<div class="image" style="background-image: url('images/menu_item_04.jpg');"></div>
+					<div class="text">Ямочный ремонт дорог</div>
+				</li>
+				<li>
+					<div class="image" style="background-image: url('images/menu_item_05.jpg');"></div>
+					<div class="text">Стоянка, парковка под ключ</div>
+				</li>
+				<li>
+					<div class="image" style="background-image: url('images/menu_item_06.jpg');"></div>
+					<div class="text">Строительство дорог</div>
+				</li>
+				<li>
+					<div class="image" style="background-image: url('images/menu_item_07.jpg');"></div>
+					<div class="text">Благоустройство территории</div>
+				</li>
+				<li>
+					<div class="image" style="background-image: url('images/menu_item_08.jpg');"></div>
+					<div class="text">Укладка асфальтовой крошки</div>
+				</li>
+				<li>
+					<div class="image" style="background-image: url('images/menu_item_09.jpg');"></div>
+					<div class="text">Продажа асфальтовой крошки</div>
+				</li>
+				<li>
+					<div class="image" style="background-image: url('images/menu_item_10.jpg');"></div>
+					<div class="text">Технология асфальтирования</div>
+				</li>
+				<li>
+					<div class="image" style="background-image: url('images/menu_item_11.jpg');"></div>
+					<div class="text">Технология строительство дорог</div>
+				</li>
+				<li>
+					<div class="image" style="background-image: url('images/menu_item_12.jpg');"></div>
+					<div class="text">Технология укладки асфальтовой крошки</div>
+				</li>
+				<li>
+					<div class="image" style="background-image: url('images/menu_item_13.jpg');"></div>
+					<div class="text">Технология ремонта дорог</div>
+				</li>
 			</ul>
 		</div>
 	</center>
